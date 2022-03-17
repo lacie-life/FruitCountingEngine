9e22d37753bc1bbce9607e81590766f005f9042a
lacie-life <00sao00ios00@gmail.com>
2022-03-17 23:53:45 +0700

    [Update]

--- Task1-DragonFruit-Detection/config.py
+++ Task1-DragonFruit-Detection/config.py
@@ -1,45 +1,24 @@
-from pprint import pprint
+import torch
 
+BATCH_SIZE = 4  # increase / decrease according to GPU memeory
+RESIZE_TO = 256  # resize the image for training and transforms
+NUM_EPOCHS = 100  # number of epochs to train for
+DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-class Config:
-    # data
-    data_dir = '/home/jun/Github/DragonFruit/archive'
-    min_size = 300  # image resize
-    max_size = 600  # image resize
-    num_workers = 1
-    test_num_workers = 1
+# training images and XML files directory
+TRAIN_DIR = 'data/train'
+# validation images and XML files directory
+VALID_DIR = 'data/test'
+# classes: 0 index is reserved for background
+CLASSES = [
+    'background', 'snake_fruit', 'dragon_fruit', 'banana', 'pineapple'
+]
 
-    # sigma for l1_smooth_loss
-    rpn_sigma = 3.
-    roi_sigma = 1.
+NUM_CLASSES = 5
 
-    # param for optimizer
-    # 0.0005 in origin paper but 0.0001 in tf-faster-rcnn
-    weight_decay = 0.0005
-    lr_decay = 0.1  # 1e-3 -> 1e-4
-    lr = 1e-3
-
-    # training
-    epoch = 14
-
-    use_adam = False  # Use Adam optimizer
-    use_chainer = False  # try match everything as chainer
-    use_drop = False  # use dropout in RoIHead
-
-    def _parse(self, kwargs):
-        state_dict = self._state_dict()
-        for k, v in kwargs.items():
-            if k not in state_dict:
-                raise ValueError('UnKnown Option: "--%s"' % k)
-            setattr(self, k, v)
-
-        print('======user config========')
-        pprint(self._state_dict())
-        print('==========end============')
-
-    def _state_dict(self):
-        return {k: getattr(self, k) for k, _ in Config.__dict__.items() \
-                if not k.startswith('_')}
-
-
-opt = Config()
+# whether to visualize images after crearing the data loaders
+VISUALIZE_TRANSFORMED_IMAGES = False
+# location to save model and plots
+OUT_DIR = 'outputs'
+SAVE_PLOTS_EPOCH = 2  # save loss plots after these many epochs
+SAVE_MODEL_EPOCH = 2  # save model after these many epochs
---
+++ Task1-DragonFruit-Detection/dataset.py
@@ -0,0 +1,159 @@
+import torch
+import cv2
+import numpy as np
+import os
+import glob as glob
+from xml.etree import ElementTree as et
+from config import CLASSES, RESIZE_TO, TRAIN_DIR, VALID_DIR, BATCH_SIZE
+from torch.utils.data import Dataset, DataLoader
+from util import collate_fn, get_train_transform, get_valid_transform
+
+
+# the dataset class
+class CustomDataset(Dataset):
+    def __init__(self, dir_path, width, height, classes, transforms=None):
+        self.transforms = transforms
+        self.dir_path = dir_path
+        self.height = height
+        self.width = width
+        self.classes = classes
+
+        # get all the image paths in sorted order
+        self.image_paths = glob.glob(f"{self.dir_path}/*.jpg")
+        self.all_images = [image_path.split('/')[-1] for image_path in self.image_paths]
+        self.all_images = sorted(self.all_images)
+
+    def __getitem__(self, idx):
+        # capture the image name and the full image path
+        image_name = self.all_images[idx]
+        image_path = os.path.join(self.dir_path, image_name)
+
+        # read the image
+        image = cv2.imread(image_path)
+        # convert BGR to RGB color format
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+        image_resized = cv2.resize(image, (self.width, self.height))
+        image_resized /= 255.0
+
+        # capture the corresponding XML file for getting the annotations
+        annot_filename = image_name[:-4] + '.xml'
+        annot_file_path = os.path.join(self.dir_path, annot_filename)
+
+        boxes = []
+        labels = []
+        tree = et.parse(annot_file_path)
+        root = tree.getroot()
+
+        # get the height and width of the image
+        image_width = image.shape[1]
+        image_height = image.shape[0]
+
+        # box coordinates for xml files are extracted and corrected for image size given
+        for member in root.findall('object'):
+            # map the current object name to `classes` list to get...
+            # ... the label index and append to `labels` list
+            labels.append(self.classes.index(member.find('name').text))
+
+            # xmin = left corner x-coordinates
+            xmin = int(member.find('bndbox').find('xmin').text)
+            # xmax = right corner x-coordinates
+            xmax = int(member.find('bndbox').find('xmax').text)
+            # ymin = left corner y-coordinates
+            ymin = int(member.find('bndbox').find('ymin').text)
+            # ymax = right corner y-coordinates
+            ymax = int(member.find('bndbox').find('ymax').text)
+
+            # resize the bounding boxes according to the...
+            # ... desired `width`, `height`
+            xmin_final = (xmin / image_width) * self.width
+            xmax_final = (xmax / image_width) * self.width
+            ymin_final = (ymin / image_height) * self.height
+            yamx_final = (ymax / image_height) * self.height
+
+            boxes.append([xmin_final, ymin_final, xmax_final, yamx_final])
+
+        # bounding box to tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        # area of the bounding boxes
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        # no crowd instances
+        iscrowd = torch.zeros((boxes.shape[0],), dtype=torch.int64)
+        # labels to tensor
+        labels = torch.as_tensor(labels, dtype=torch.int64)
+
+        # prepare the final `target` dictionary
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+        image_id = torch.tensor([idx])
+        target["image_id"] = image_id
+
+        # apply the image transforms
+        if self.transforms:
+            sample = self.transforms(image=image_resized,
+                                     bboxes=target['boxes'],
+                                     labels=labels)
+            image_resized = sample['image']
+            target['boxes'] = torch.Tensor(sample['bboxes'])
+
+        return image_resized, target
+
+    def __len__(self):
+        return len(self.all_images)
+
+
+# prepare the final datasets and data loaders
+train_dataset = CustomDataset(TRAIN_DIR, RESIZE_TO, RESIZE_TO, CLASSES, get_train_transform())
+valid_dataset = CustomDataset(VALID_DIR, RESIZE_TO, RESIZE_TO, CLASSES, get_valid_transform())
+
+train_loader = DataLoader(
+    train_dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+    num_workers=0,
+    collate_fn=collate_fn
+)
+
+valid_loader = DataLoader(
+    valid_dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=False,
+    num_workers=0,
+    collate_fn=collate_fn
+)
+print(f"Number of training samples: {len(train_dataset)}")
+print(f"Number of validation samples: {len(valid_dataset)}\n")
+
+# execute datasets.py using Python command from Terminal...
+# ... to visualize sample images
+# USAGE: python datasets.py
+if __name__ == '__main__':
+    # sanity check of the Dataset pipeline with sample visualization
+    dataset = CustomDataset(
+        TRAIN_DIR, RESIZE_TO, RESIZE_TO, CLASSES
+    )
+    print(f"Number of training images: {len(dataset)}")
+
+    # function to visualize a single sample
+    def visualize_sample(image, target):
+        box = target['boxes'][0]
+        label = CLASSES[target['labels']]
+        cv2.rectangle(
+            image,
+            (int(box[0]), int(box[1])), (int(box[2]), int(box[3])),
+            (0, 255, 0), 1
+        )
+        cv2.putText(
+            image, label, (int(box[0]), int(box[1] - 5)),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2
+        )
+        cv2.imshow('Image', image)
+        cv2.waitKey(0)
+
+
+    NUM_SAMPLES_TO_VISUALIZE = 5
+    for i in range(NUM_SAMPLES_TO_VISUALIZE):
+        image, target = dataset[i]
+        visualize_sample(image, target)
