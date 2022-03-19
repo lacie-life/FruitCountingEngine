466ae42f0bf2510e39cd7c6243c8ee5ca9bb7d01
lacie-life <00sao00ios00@gmail.com>
2022-03-19 13:16:38 +0700

    [Update]

--- Task1-DragonFruit-Detection/model.py
+++ Task1-DragonFruit-Detection/model.py
@@ -21,39 +21,42 @@ def create_model(num_classes):
 
 def convert_to_onnx(model_path, img_path):
 
+    # Image sample input
     image = cv2.imread(img_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
-
+    image_resized = cv2.resize(image, (256, 256))
     # make the pixel range between 0 and 1
-    image /= 255.0
+    image_resized /= 255.0
     # bring color channels to front
-    image = np.transpose(image, (2, 0, 1)).astype(float)
+    image_resized = np.transpose(image_resized, (2, 0, 1)).astype(float)
     # convert to tensor
-    image = torch.tensor(image, dtype=torch.float)
+    image_resized = torch.tensor(image_resized, dtype=torch.float)
     # add batch dimension
-    image = torch.unsqueeze(image, 0)
+    image_resized = torch.unsqueeze(image_resized, 0)
 
+    # Model
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-    model = create_model(num_classes=5).to('cuda')
-
-    model.load_state_dict(torch.load(model_path, map_location='cuda'))
+    model = create_model(num_classes=5).to(device)
+    state_dict = torch.load(model_path, map_location='cuda')
+    model.load_state_dict(state_dict)
     model.eval()
-    model.to('cuda')
 
-    image = image.to('cuda')
-
-    inputs = torch.randn(1, 3, 300, 400, device="cuda")
-    inputs = inputs.to('cuda')
+    model.to(device)
+    onnx_input = image_resized.cuda()
 
     with torch.no_grad():
-        outputs = model(image)
+        outputs = model(onnx_input)
+
+    print(next(model.parameters()).device)
+    print(onnx_input.device)
 
     torch.onnx.export(
-        model, inputs,
+        model, onnx_input,
         ONNX_FILE_PATH,
-        input_names=["input"], output_names=["output"],
+        input_names=["input"],
+        output_names=["output"],
         export_params=True,
+        verbose=True,
         opset_version=11)
     onnx_model = onnx.load(ONNX_FILE_PATH)
     # check that the model converted fine
