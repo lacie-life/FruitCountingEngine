0cac660c3c7395e476d711b8d9083db86957e873
lacie-life <00sao00ios00@gmail.com>
2022-03-24 16:44:43 +0700

    [Update]

---
+++ Task1-DragonFruit-Detection/faster-rcnn/inference.py
@@ -0,0 +1,84 @@
+import numpy as np
+import cv2
+import torch
+import glob as glob
+import time
+
+from model import create_model
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+model = create_model(num_classes=5).to(device)
+
+model.load_state_dict(torch.load("outputs/model100.pth", map_location=device))
+
+model.eval()
+
+# directory where all the images are present
+DIR_TEST = 'data/test'
+test_images = glob.glob(f"{DIR_TEST}/images/*.png")
+
+print(f"Test instances: {len(test_images)}")
+
+print(test_images[0])
+
+# classes: 0 index is reserved for background
+CLASSES = [
+    'background', 'snake fruit', 'dragon fruit', 'banana', 'pineapple',
+]
+
+# define the detection threshold...
+# ... any detection having score below this will be discarded
+detection_threshold = 0.8
+
+for i in range(len(test_images)):
+    ts = time.time()
+    # get the image file name for saving output later on
+    image_name = test_images[i].split('/')[-1].split('.')[0]
+    image = cv2.imread(test_images[i])
+    orig_image = image.copy()
+    # BGR to RGB
+    image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB).astype(np.float32)
+    # make the pixel range between 0 and 1
+    image /= 255.0
+    # bring color channels to front
+    image = np.transpose(image, (2, 0, 1)).astype(float)
+    # convert to tensor
+    image = torch.tensor(image, dtype=torch.float).cuda()
+    # add batch dimension
+    image = torch.unsqueeze(image, 0)
+    with torch.no_grad():
+        outputs = model(image)
+
+    # load all detection to CPU for further operations
+    outputs = [{k: v.to('cpu') for k, v in t.items()} for t in outputs]
+    # carry further only if there are detected boxes
+    ts_ = time.time()
+    if len(outputs[0]['boxes']) != 0:
+        boxes = outputs[0]['boxes'].data.numpy()
+        scores = outputs[0]['scores'].data.numpy()
+        # filter out boxes according to `detection_threshold`
+        boxes = boxes[scores >= detection_threshold].astype(np.int32)
+        draw_boxes = boxes.copy()
+        # get all the predicited class names
+        pred_classes = [CLASSES[i] for i in outputs[0]['labels'].cpu().numpy()]
+
+        # draw the bounding boxes and write the class name on top of it
+        for j, box in enumerate(draw_boxes):
+            cv2.rectangle(orig_image,
+                          (int(box[0]), int(box[1])),
+                          (int(box[2]), int(box[3])),
+                          (0, 0, 255), 2)
+            cv2.putText(orig_image, pred_classes[j],
+                        (int(box[0]), int(box[1] - 5)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0),
+                        2, lineType=cv2.LINE_AA)
+        cv2.imshow('Prediction', orig_image)
+        cv2.waitKey(1)
+        cv2.imwrite(f"test_predictions/{image_name}.jpg", orig_image, )
+    print(f"Image {i + 1} done...")
+    print('-' * 50)
+    print("Time: ")
+    print(ts_ - ts)
+print('TEST PREDICTIONS COMPLETE')
+cv2.destroyAllWindows()
