00965f257fc61797912c8b7d00d10c9a927c5d6d
lacie-life <00sao00ios00@gmail.com>
2022-04-02 22:56:35 +0700

    [Update] update bbtracker

--- Task4-Counting-Dragon-Fruit/BBTracker/BBTracker/include/bbtracker.h
+++ Task4-Counting-Dragon-Fruit/BBTracker/BBTracker/include/bbtracker.h
@@ -5,8 +5,36 @@
 #ifndef BBTRACKER_H
 #define BBTRACKER_H
 
+#include <opencv2/opencv.hpp>
+#include <math.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <condition_variable>
 
-class BBTracker {
+#include "pipeline.h"
+#include "detectNet.h"
+#include "videoSource.h"
+#include "videoOutput.h"
+#include "Ctracker.h"
+
+
+class BBTracker : public pipeline{
+public:
+    BBTracker(commandLine cmdLine);
+
+private:
+    detectNet* net;
+
+protected:
+    std::vector<vector<float>> detectFrame(cv::Mat frame);
+    void DrawData(cv::Mat frame, int framesCounter, double fontScale);
+    void DrawCounter(cv::Mat frame, double fontScale, std::map <string,  int> &countObjects_LefttoRight, std::map <string,  int> &countObjects_RighttoLeft);
+    void CounterUpdater(cv::Mat frame, std::map <string,  int> &countObjects_LefttoRight, std::map <string,  int> &countObjects_RighttoLeft);
 
 };
 
