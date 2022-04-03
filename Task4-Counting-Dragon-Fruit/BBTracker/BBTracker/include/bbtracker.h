b593ea69999c61253eace0c0ce7a6157654b022d
lacie-life <00sao00ios00@gmail.com>
2022-04-03 16:41:08 +0700

    [Pending]

--- Task4-Counting-Dragon-Fruit/BBTracker/BBTracker/include/bbtracker.h
+++ Task4-Counting-Dragon-Fruit/BBTracker/BBTracker/include/bbtracker.h
@@ -16,16 +16,29 @@
 #include <chrono>
 #include <condition_variable>
 
-#include "pipeline.h"
+#include "defines.h"
 #include "detectNet.h"
 #include "videoSource.h"
 #include "videoOutput.h"
 #include "Ctracker.h"
 
+using namespace std;
+using namespace cv;
 
-class BBTracker : public pipeline{
+DEFINE_string(mean_file, "",
+              "The mean file used to subtract from the input image.");
+
+DEFINE_string(mean_value, "104,117,123",
+              "If specified, can be one value or can be same as image channels"
+              " - would subtract from the corresponding channel). Separated by ','."
+              "Either mean_file or mean_value should be provided, not both.");
+
+
+class BBTracker{
 public:
     BBTracker(commandLine cmdLine, const cv::CommandLineParser &parser);
+
+    void Process();
 
 private:
     detectNet* net;
@@ -33,6 +46,22 @@ private:
 
     std::string meanFile;
     std::string meanValue;
+
+    bool saveVideo;
+    bool drawCount;
+    bool drawOther;
+    bool useCrop;
+    int endFrame;
+    int startFrame;
+    cv::Rect cropRect;
+    bool desiredDetect;
+    int cropFrameWidth;
+    std::string inFile;
+    std::string outFile;
+    int cropFrameHeight;
+    float detectThreshold;
+    std::vector<cv::Scalar> m_colors;
+    std::string desiredObjectsString;
 
     int line1_x1;
     int line1_x2;
@@ -44,10 +73,21 @@ private:
     int line2_y2;
 
 protected:
+    std::unique_ptr<CTracker> m_tracker;
+    float m_fps;
+    bool enableCount;
+    int direction;
+
     std::vector<vector<float>> detectFrame(cv::Mat frame);
     void DrawData(cv::Mat frame, int framesCounter, double fontScale);
     void DrawCounter(cv::Mat frame, double fontScale, std::map <string,  int> &countObjects_LefttoRight, std::map <string,  int> &countObjects_RighttoLeft);
     void CounterUpdater(cv::Mat frame, std::map <string,  int> &countObjects_LefttoRight, std::map <string,  int> &countObjects_RighttoLeft);
+    void DrawTrack(cv::Mat frame,
+                   int resizeCoeff,
+                   const CTrack& track,
+                   bool drawTrajectory = true,
+                   bool isStatic = false);
+    double CalculateRelativeSize(int frame_width, int frame_height);
 
 };
 
